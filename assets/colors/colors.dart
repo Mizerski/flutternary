6c85a5bb681f32184627e32a20ddaef2f6b85f30
João Victor Carvalho <layelcode@gmail.com>
2022-12-28 01:27:28 -0300

    feat: colors palet

---
+++ assets/colors/colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+final bgColor = Color(0x484848);
+
+// Text Color
+final headerTextColor = Color(0x000000);
+
+final textColor = Color(0x000000);
+
+final boldOrangeColor = Color(0xD36500);
+
+final boldGreenColor = Color(0x013900);
+
+final boldGreenYellowColor = Color(0x878200);
+
+final boldBlueColor = Color(0x0468C3);
+
+// Search Color
+final bgSearchColor = Color(0xD7CFCF);
+
+// Items Color
+final bgItemsColor = Color(0xD9D9D9);
+
+final borderItemsColor = Color(0xB9B9B9);
+
+// Cards Color
+final bgCardsColor = Color(0xD9D9D9);
+
+// Nav Bar Color
+final navBarColor = Color(0xC8C4C4);
